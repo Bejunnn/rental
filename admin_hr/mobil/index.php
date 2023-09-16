e62e289d53e930e012a5606969af7fd02443fedd
Bejunnn <arjunheriyanto@gmail.com>
2023-09-16 09:11:48 +0700

    terbaru

--- admin_hr/mobil/index.php
+++ admin_hr/mobil/index.php
@@ -138,7 +138,7 @@ if (isset($_SESSION['sebagai'])) {
                     <li class="nav-item dropdown no-arrow">
                         <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?= $_SESSION['nama_engkap']; ?></span>
+                            <span class="mr-2 d-none d-lg-inline text-gray-600 small"> <?= $_SESSION['nama_lengkap']; ?></span>
                             <img class="img-profile rounded-circle"
                                 src="../../assets/img/undraw_profile.svg">
                         </a>
