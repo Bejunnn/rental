67abf4fdabb0a08703561dc668769b8e96a06750
Bejunnn <arjunheriyanto@gmail.com>
2023-09-23 08:41:42 +0700

    mysql terbru

--- rent_mobil (2).sql
+++ rent_mobil (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 16 Sep 2023 pada 04.18
+-- Waktu pembuatan: 23 Sep 2023 pada 03.40
 -- Versi server: 10.4.28-MariaDB
 -- Versi PHP: 8.2.4
 
@@ -47,7 +47,9 @@ INSERT INTO `login` (`id`, `nama_lengkap`, `no_telp`, `alamat_email`, `password`
 (11, 'bagus', '097341837', 'bagus@gmail.com', '11111111', 'karyawan'),
 (12, 'suparmin', '097373636', 'supra@gmail.com', '88888888', 'karyawan'),
 (13, 'epi', '0873636172', 'epi@gmail.com', '99999999', 'karyawan'),
-(14, 'abi', '123', 'abi@gmail.com', 'abi', 'ga');
+(14, 'abi', '123', 'abi@gmail.com', 'abi', 'ga'),
+(15, 'Arjun', '0986382', 'jun@gmail.com', 'junn', 'ga'),
+(16, 'botol', '098790', 'apis@gmail.com', '987654321', 'karyawan');
 
 -- --------------------------------------------------------
 
@@ -61,16 +63,17 @@ CREATE TABLE `mobil` (
   `no_polisi` varchar(50) NOT NULL,
   `jumlah_kursi` int(20) NOT NULL,
   `tahun_beli` int(20) NOT NULL,
-  `gambar` varchar(255) NOT NULL
+  `gambar` varchar(255) NOT NULL,
+  `status` int(4) NOT NULL DEFAULT 0 COMMENT '1 Active = 2 InActive'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `mobil`
 --
 
-INSERT INTO `mobil` (`id`, `nama_mobil`, `no_polisi`, `jumlah_kursi`, `tahun_beli`, `gambar`) VALUES
-(2, 'suzuko', 'b 4386 hdf', 6, 2018, '812-Gambar WhatsApp 2023-09-11 pukul 08.55.29.jpg'),
-(3, 'kijang', 'b 4863 jds', 4, 2010, '675-495521.jpg');
+INSERT INTO `mobil` (`id`, `nama_mobil`, `no_polisi`, `jumlah_kursi`, `tahun_beli`, `gambar`, `status`) VALUES
+(2, 'suzki', 'b 4386 hdf', 6, 2018, '576-Gambar WhatsApp 2023-09-11 pukul 08.55.29.jpg', 0),
+(3, 'kijang', 'b 4863 jds', 4, 2010, '675-495521.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -83,26 +86,26 @@ CREATE TABLE `permintaan` (
   `nama_mobil` varchar(100) NOT NULL,
   `nama_pemesan` varchar(50) NOT NULL,
   `kota_tujuan` varchar(50) NOT NULL,
+  `pengeluaran` varchar(50) NOT NULL,
   `no_polisi` varchar(50) NOT NULL,
   `tanggal_pinjam` date NOT NULL,
   `tanggal_kembali` date NOT NULL,
-  `status` int(10) NOT NULL
+  `status_perjalanan` int(50) NOT NULL,
+  `status_pengeluaran` int(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `permintaan`
 --
 
-INSERT INTO `permintaan` (`id_permintaan`, `nama_mobil`, `nama_pemesan`, `kota_tujuan`, `no_polisi`, `tanggal_pinjam`, `tanggal_kembali`, `status`) VALUES
-(9, 'r3', '', '', '4', '2023-09-12', '2023-09-13', 0),
-(10, 'r3', '', '', '4', '2023-09-12', '2023-09-14', 0),
-(11, 'suzuko', '', 'nldksac', 'b 4386 hdf', '2023-09-15', '2023-09-15', 0),
-(12, 'kijang', '', 'njsdc', 'b 4386 hdf', '2023-09-29', '2023-09-30', 0),
-(13, 'suzuko', '', 'hjgk', 'b 4386 hdf', '2023-09-15', '2023-09-19', 0),
-(14, 'kijang', '', 'jluh', 'b 4386 hdf', '2023-10-04', '2023-10-05', 0),
-(15, 'kijang', '', 'bogor', 'b 4386 hdf', '2023-09-22', '2023-09-25', 0),
-(16, 'kijang', '', 'bogor', 'b 4386 hdf', '2023-09-15', '2023-09-15', 0),
-(17, 'suzuko', 'epi', 'bogor', 'b 4386 hdf', '2023-09-15', '2023-10-01', 0);
+INSERT INTO `permintaan` (`id_permintaan`, `nama_mobil`, `nama_pemesan`, `kota_tujuan`, `pengeluaran`, `no_polisi`, `tanggal_pinjam`, `tanggal_kembali`, `status_perjalanan`, `status_pengeluaran`) VALUES
+(12, 'kijang', '', 'njsdc', '', 'b 4386 hdf', '2023-09-29', '2023-09-30', 0, 0),
+(13, 'suzuko', '', 'hjgk', '', 'b 4386 hdf', '2023-09-15', '2023-09-19', 0, 0),
+(14, 'kijang', '', 'jluh', '', 'b 4386 hdf', '2023-10-04', '2023-10-05', 0, 0),
+(15, 'kijang', '', 'bogor', '', 'b 4386 hdf', '2023-09-22', '2023-09-25', 0, 0),
+(16, 'kijang', '', 'bogor', '', 'b 4386 hdf', '2023-09-15', '2023-09-15', 0, 0),
+(17, 'suzuko', 'epi', 'bogor', '', 'b 4386 hdf', '2023-09-15', '2023-10-01', 0, 0),
+(18, 'kijang', 'epi', 'bogor', '', 'b 4386 hdf', '2023-10-07', '2023-10-07', 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -134,7 +137,7 @@ ALTER TABLE `permintaan`
 -- AUTO_INCREMENT untuk tabel `login`
 --
 ALTER TABLE `login`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT untuk tabel `mobil`
@@ -146,7 +149,7 @@ ALTER TABLE `mobil`
 -- AUTO_INCREMENT untuk tabel `permintaan`
 --
 ALTER TABLE `permintaan`
-  MODIFY `id_permintaan` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id_permintaan` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
