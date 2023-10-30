773e1eafe778e741880500341091c540d015011a
Wilenn <waylend07@gmail.com>
2023-10-30 21:35:57 +0700

    do

--- hr/rekapitulasi/export_akomodasi.php
+++ hr/rekapitulasi/export_akomodasi.php
@@ -5,28 +5,22 @@ $rows = [];
 while ($row = mysqli_fetch_assoc($result)) {
     $rows[] = $row;
 }
+
 session_start();
 if (!isset($_SESSION['sebagai'])) {
     header("Location: ../index.php");
 }
 
 if (isset($_SESSION['sebagai'])) {
-    if ($_SESSION['sebagai'] == 'ga') {
-        header('Location: ga.php');
-        exit;
-    } elseif ($_SESSION['sebagai'] == 'karyawan') {
-        header("Location: karyawan.php");
-        exit;
-    } elseif ($_SESSION['sebagai'] == 'admin_hr') {
-        header("Location: admin_hr.php");
-        exit;
+    if ($_SESSION['sebagai'] == 'user') {
+        header('Location: ./user.php');
     }
 }
 ?>
 <html>
 
 <head>
-    <title>Stock Barang</title>
+    <title>Data Absensi</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
@@ -36,39 +30,38 @@ if (isset($_SESSION['sebagai'])) {
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.js"></script>
 </head>
+<br>
 
 <body>
     <div class="container">
         <h2>Data Akomodasi</h2>
+        <h4></h4>
         <div class="data-tables datatable-dark">
-
-            <table class="table table-bordered" id="mauexport" cellspacing="0">
+            <table class="table table-bordered" id="mauexport" width="100%" cellspacing="0">
                 <thead>
                     <tr>
-                        <th>No</th>
                         <th>Nama Pemesan</th>
                         <th>Kota Tujuan</th>
                         <th>Kendaraan</th>
                         <th>Pengeluaran</th>
                     </tr>
                 </thead>
-                <?php
-                $no = '1';
-                foreach ($rows as $data) {
-                ?>
-                    <tbody>
+                <tbody>
+                    <?php foreach ($rows as $data) {
+                    ?>
                         <tr>
-                            <td><?= $no++ ?></td>
                             <td><?= $data['nama_pemesan']; ?></td>
                             <td><?= $data['kota_tujuan']; ?></td>
                             <td><?= $data['kendaraan']; ?></td>
                             <td><?= $data['pengeluaran']; ?></td>
+
                         </tr>
                     <?php
-                }
+                    }
                     ?>
-                    </tbody>
+                </tbody>
             </table>
+
         </div>
     </div>
 
@@ -77,7 +70,7 @@ if (isset($_SESSION['sebagai'])) {
             $('#mauexport').DataTable({
                 dom: 'Bfrtip',
                 buttons: [
-                    'excel'
+                    'excel', 'print'
                 ]
             });
         });
